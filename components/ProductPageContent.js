57552d0859a8cf076cf450447ceb2280c699d5be
Hgahlla <harmohangahlla@gmail.com>
2022-10-12 14:23:52 -0500

    Added product details (image and options) to the product page

--- components/ProductPageContent.js
+++ components/ProductPageContent.js
@@ -1,5 +1,27 @@
+import Image from "next/image";
+import ProductForm from "./ProductForm";
+
 const ProductPageContent = ({ product }) => {
-  return <div>{product.title}</div>;
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center w-11/12 max-w-6xl mx-auto space-y-8 md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8">
+        <div className="w-full max-w-md overflow-hidden bg-white border shadow-lg rounded-2xl md:w-1/2">
+          <div className="relative w-full h-96">
+            <Image
+              src={product.images.edges[0].node.url}
+              alt={product.images.edges[0].node.altText}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        </div>
+        <ProductForm product={product} />
+      </div>
+      <p className="w-11/12 max-w-3xl pt-16 mx-auto space-y-8 md:space-x-4 lg:space-x-8">
+        {product.description}
+      </p>
+    </>
+  );
 };
 
 export default ProductPageContent;
