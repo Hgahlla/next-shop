d4f4a8eedf5696179aad1af09e3b9217a69dfbc2
Hgahlla <harmohangahlla@gmail.com>
2022-10-14 16:07:52 -0500

    Implemented  an image carousel for product page

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,9 @@ import { useRouter } from "next/router";
 import Layout from "../components/Layout";
 import ShopProvider from "../context/shopContext";
 import "tailwindcss/tailwind.css";
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
 
 const MyApp = ({ Component, pageProps }) => {
   const router = useRouter();
